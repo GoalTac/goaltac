f158fadc4c302245e4eeb1e83850e2012e6cec32
Wrys <wrysplays@gmail.com>
2023-11-24 20:13:07 -0500

    Signup UI Changes

--- src/components/sign-up-form.tsx
+++ src/components/sign-up-form.tsx
@@ -114,6 +114,24 @@ export function SignUpForm() {
         </div>
       </form>
       </Form>
+
+      <p className="px-8 text-center text-sm text-muted-foreground">
+        By clicking continue, you agree to our{" "}
+        <Link
+          href="/terms"
+          className="underline underline-offset-4 hover:text-primary"
+        >
+          Terms of Service
+        </Link>{" "}
+        and{" "}
+        <Link
+          href="/privacy"
+          className="underline underline-offset-4 hover:text-primary"
+        >
+          Privacy Policy
+        </Link>
+        .
+      </p>
     </div>
   );
 }
