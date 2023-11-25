a584d4fff8e8791f698f59b708bd7bc08c49233c
Wrys <wrysplays@gmail.com>
2023-11-25 17:21:29 -0500

    Removed getSession and getUser

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,20 +1,20 @@
-import { AuthError } from "@supabase/supabase-js";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { supabase } from "~/utils/supabaseClient";
 
 export const authRouter = createTRPCRouter({
   signUpEmail: publicProcedure
     .input(z.object({ email: z.string(), password: z.string(), }))
     .mutation(async({ input }) => {
+        const supabase = useSupabaseClient()
 
         const { data, error } = await supabase.auth.signUp({
             email: input.email,
             password: input.password,
             options: {
-                emailRedirectTo: 'https://localhost:3000/dashboard'
+                emailRedirectTo: "http://localhost:3000/auth/callback"
             }
         })
 
@@ -29,34 +29,15 @@ export const authRouter = createTRPCRouter({
         }
   
         if(data.session) await supabase.auth.setSession(data.session)
-
         return { data, error, userExists };
     }),
 
     signInEmail: publicProcedure
     .input(z.object({ email: z.string(), password: z.string(), }))
     .mutation(async({ input }) => {
+        const supabase = useSupabaseClient()
+
         const { data, error } = await supabase.auth.signInWithPassword(input)
-
-        if(error) {
-            throw new TRPCError({code: 'UNAUTHORIZED', message: error.message})
-        }
-
-        return { data, error };
-    }),
-    getSession: publicProcedure
-    .query(async() => {
-        const { data, error } = await supabase.auth.getSession()
-
-        if(error) {
-            throw new TRPCError({code: 'UNAUTHORIZED', message: error.message})
-        }
-
-        return { data, error };
-    }),
-    getUser: publicProcedure
-    .query(async() => {
-        const { data, error } = await supabase.auth.getUser()
 
         if(error) {
             throw new TRPCError({code: 'UNAUTHORIZED', message: error.message})
@@ -66,10 +47,12 @@ export const authRouter = createTRPCRouter({
     }),
     signOut: publicProcedure
     .mutation(async() => {
+        const supabase = useSupabaseClient()
+
         const { error } = await supabase.auth.signOut()
 
         if(error) {
-            throw new AuthError(error.message)
+            throw new TRPCError({code: 'UNAUTHORIZED', message: error.message})
         }
 
         return error;
