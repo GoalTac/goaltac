f474f0580021b395fc82f168eec210c1a21a8f53
Wrys <wrysplays@gmail.com>
2023-11-30 11:49:27 -0500

    Finishing up the page UI

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,8 +15,15 @@ const config = {
     // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unsafe-return" : 'warn',
+    "@typescript-eslint/no-unsafe-member-access" : "warn",
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/no-unsafe-argument" : "warn",
+    "@typescript-eslint/no-unused-vars" : "warn",
+    "@typescript-eslint/ban-types": "warn",
+    "@typescript-eslint/no-namespace": "warn",
+    "@typescript-eslint/no-empty-interface" : "warn",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
@@ -24,7 +31,6 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-misused-promises": [
       2,
       {
