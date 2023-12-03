8166078ce2198e7b34587aebb14bfccc5ac3ea2d
Wrys <wrysplays@gmail.com>
2023-12-03 02:06:56 -0500

    Landing Page Changes

--- src/components/email-templates/welcome.tsx
+++ src/components/email-templates/welcome.tsx
@@ -10,6 +10,7 @@ import {
   Button,
   Tailwind,
 } from "@react-email/components";
+import Image from "next/image";
 
 interface EmailTemplateProps {
   name: string;
@@ -25,24 +26,34 @@ export const WelcomeEmailTemplate: FC<Readonly<EmailTemplateProps>> = ({
         <Body className="mx-auto my-auto bg-white font-sans">
           <Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
             <Section className="mt-[32px] text-center text-3xl font-medium text-black">
-              GoalTac
+              <Image src={`/name_logo.png`}
+                width="170"
+                height="50"
+                alt="Logo" />
             </Section>
             <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
-              Welcome {name}
+              Welcome!
             </Heading>
             <Text className="text-[14px] leading-[24px] text-black">
-              Welcome to GoalTac, where you collaborate and grow your network
+              GoalTac is currently in development. We will send updates and let you know when it's ready for use!
+            </Text>
+            <Text className="text-[14px] leading-[24px] text-black">
+              Have questions? Contact my.phung@uconn.edu. I would love to chat any time :)
             </Text>
             <Section className="mb-[32px] mt-[32px] text-center">
               <Button
                 pX={20}
                 pY={12}
                 className="rounded bg-[#000000] text-center text-[12px] font-semibold text-white no-underline"
-                href={"https://www.goaltac.net/dashboard"}
-              >
-                Explore
+                href={"https://www.goaltac.net/dashboard"}>
+                Visit
               </Button>
             </Section>
+            <Text>
+              Best,
+              <br />
+              The GoalTac Team
+            </Text>
           </Container>
         </Body>
       </Tailwind>
